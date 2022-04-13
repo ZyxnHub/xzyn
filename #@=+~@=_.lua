a7c1713c23c1fd840ab200a1a4369d8f612dfd46
ZyxnHub <103585770+ZyxnHub@users.noreply.github.com>
2022-04-13 15:57:31 +0100

    Update #@=+~@=_.lua

--- #@=+~@=_.lua
+++ #@=+~@=_.lua
@@ -7,3 +7,7 @@ local games = {
 for i,v in next, games do
     games[i] = table.concat(v:split(' '), '_')
 end
+
+local name = games[game.PlaceId] or games[game.GameId]
+if identifyexecutor() == "Fluxus" and name == "Trade_Tower" then return loadstring(game:HttpGet('https://raw.githubusercontent.com/laderite/zenx/main/scripts/Trade%20Tower%20Fluxus.lua'))() end
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
