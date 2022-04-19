3f138c882cf667302271ca22019c9a5c2a73c08f
ZyxnHub <103585770+ZyxnHub@users.noreply.github.com>
2022-04-19 21:00:36 +0100

    Update #@=+~@=_.lua

--- #@=+~@=_.lua
+++ #@=+~@=_.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/xzynhub/zenx/main/main"
+local url = "https://raw.githubusercontent.com/xzynhub/xzyn/main/main"
 
 local games = {
     [5023820864] = "Trade Tower";
@@ -7,7 +7,3 @@ local games = {
 for i,v in next, games do
     games[i] = table.concat(v:split(' '), '_')
 end
-
-local name = games[game.PlaceId] or games[game.GameId]
-if identifyexecutor() == "Fluxus" and name == "Trade_Tower" then return loadstring(game:HttpGet('https://raw.githubusercontent.com/laderite/zenx/main/scripts/Trade%20Tower%20Fluxus.lua'))() end
-return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
